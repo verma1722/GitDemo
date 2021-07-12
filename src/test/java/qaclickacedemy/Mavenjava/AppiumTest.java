677b15e364de9aef5db552a0eb9e251e55bab3e4
abhinav <abhinavvermarbs@gmail.com>
2021-07-13 00:33:23 +0530

    architect assian development

--- src/test/java/qaclickacedemy/Mavenjava/AppiumTest.java
+++ src/test/java/qaclickacedemy/Mavenjava/AppiumTest.java
@@ -7,6 +7,9 @@ public class AppiumTest {
 	public void Appium()
 	{
 		System.out.println("Appium");
+		System.out.println("Appium1");
+		System.out.println("Appium2");
+		System.out.println("Appium3");
 	}
 	@Test
 	public void test()
