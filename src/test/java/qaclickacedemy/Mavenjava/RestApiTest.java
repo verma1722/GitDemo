92904ef7fb40d84a71cfe2b36868652bbc35ad6f
abhinav <abhinavvermarbs@gmail.com>
2021-07-12 23:21:27 +0530

    this my again commit2

--- src/test/java/qaclickacedemy/Mavenjava/RestApiTest.java
+++ src/test/java/qaclickacedemy/Mavenjava/RestApiTest.java
@@ -7,14 +7,11 @@ public class RestApiTest {
 	public void Rest()
 	{
 		System.out.println("Rest");	
-		System.out.println("Rest1");	
-		System.out.println("Rest3");	
 	}
 	@Test
 	public void Api()
 	{
 		System.out.println("Api");
-		System.out.println("Api2");
 	}
 
 }
