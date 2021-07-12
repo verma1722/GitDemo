107aac07e9c091e8e9635c7d2b822237d7d09d28
abhinav <abhinavvermarbs@gmail.com>
2021-07-12 19:13:44 +0530

    this my first commit update done

--- src/test/java/qaclickacedemy/Mavenjava/RestApiTest.java
+++ src/test/java/qaclickacedemy/Mavenjava/RestApiTest.java
@@ -7,12 +7,13 @@ public class RestApiTest {
 	public void Rest()
 	{
 		System.out.println("Rest");	
+		System.out.println("Rest1");	
+		System.out.println("Rest3");	
 	}
 	@Test
 	public void Api()
 	{
 		System.out.println("Api");
 	}
-
 
 }
